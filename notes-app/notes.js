59afb23e980143a65a35ba74e38b8b740b69632f
Hillary Sousa <hillaryhpf@gmail.com>
2019-09-05 19:25:08 -0300

    Notes app: removendo node_modules do repositório e adicionando funcionalidade para adicionar notas

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,45 @@
+const fs = require('fs');
+
+const loadNotes = function () {
+    try {
+        const dataBuffer = fs.readFileSync('data.json');
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON);
+    } catch (e) {
+        return [];
+    };
+}
+
+const saveNotes = function (notes) {
+    const data = JSON.stringify(notes);
+    fs.writeFileSync('data.json', data);
+}
+
 const getNotes = function () {
     return "Your notes...";
 }
 
-module.exports = getNotes;
+const addNote = function (title, body) {
+    const notes = loadNotes();
+    const duplicateNotes = notes.filter(function (note) {
+        return note.title === title;
+    })
+
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        });
+
+        saveNotes(notes);
+
+        console.log('New note added!');
+    } else {
+        console.log('This note title already exists. Please write a different title.');
+    }
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote
+};
